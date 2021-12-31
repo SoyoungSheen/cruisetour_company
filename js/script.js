b0742ab4b02779d525482d758d40b27d4eeb98e9
SoyoungSheen <sheen1210@gmail.com>
2021-12-31 15:59:17 +0900

    dd

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0";
+}
+
+
 window.onload = function () {
-
-  function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-  }
-
-  function closeNav() {
-    document.getElementById("myNav").style.width = "0";
-  }
 
   document.getElementsByClassName('category')[0].addEventListener("mouseover", pic);
 
